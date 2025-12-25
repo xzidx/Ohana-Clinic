1592ab8bac3f4bb51859103ddb7ba82ecc2faf7f
xzidx <c.samnang.de@gmail.com>
2025-12-25 10:14:09 +0700

    10 more

--- wp-content/themes/ohnaclinic/page-about.php
+++ wp-content/themes/ohnaclinic/page-about.php
@@ -1,1 +1,18 @@
-<?php get_header(); ?>
+<?php
+get_header();
+?>
+
+<main>
+    <h1><?php the_title(); ?></h1>
+    <div class="page-content">
+        <?php
+        while ( have_posts() ) : the_post();
+            the_content();
+        endwhile;
+        ?>
+    </div>
+</main>
+
+<?php
+get_footer();
+?>
