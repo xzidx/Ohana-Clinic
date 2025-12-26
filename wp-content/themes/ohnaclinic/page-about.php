fc43c09786d93da79c6a0c400cd649da85deaab1
punloeusok420 <punloeusok01@gmail.com>
2025-12-26 13:45:04 +0700

    10 more yes

--- wp-content/themes/ohnaclinic/page-about.php
+++ wp-content/themes/ohnaclinic/page-about.php
@@ -1,25 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
 /*
 Template Name: About Page
@@ -27,15 +6,86 @@ Template Name: About Page
 get_header();
 ?>
 
+
 <main>
-    <h1><?php the_title(); ?></h1>
-    <div class="page-content">
-        <?php
-        while ( have_posts() ) : the_post();
-            the_content();
-        endwhile;
-        ?>
-    </div>
+   <div class="container">
+                  <div class="about-img">
+                  <img src="http://clinic_website.test/wp-content/uploads/2025/12/surgin-room-scaled.jpeg" alt="">
+                  </div>
+                           <div class="hero-text">
+                                  <h2>Our hospital philosophy</h2>
+                          </div>
+
+                          <div class="line-letter">
+                          </div>
+                  <div class="container-2">
+                        <div class="title">
+                                  <h3>"More friendly infertility treatment"</h3>
+                         </div>
+
+                            <div class="paragrap">
+
+                                 <span>These words express our desire to encourage communication and friendship between  <br>patients, 
+                                       doctors, and staff, so that infertility treatment, which can often be a gloomy experience, <br>can be carried 
+                                       out in a positive manner. Our approximately 40 staff members will
+                                       do their utmost to support <br>
+                                       as many patients as possible so that they can have children. 
+                                       Our clinic is a place of care, comfort, and modern excellence. <br>
+                                       Every patient receives gentle, professional treatment from the heart.
+                                       We believe that healing begins the moment you walk through our doors.
+                                    </span>
+
+                            </div>
+
+                     <div class="title2">
+                     <h3>"Easy-to-access medical system"</h3>
+                    </div>
+
+                    <div class="box">
+
+                        <div class="box1">
+                           <span>Located a one-minute walk from  <br>Ikebukuro 
+                                 Exit 35, our clinic is open weekdays from <br>
+                                 8:00 AM to 8:00 PM (Mondays, Wednesdays,<br>
+                                 and Fridays), including weekends and <br>
+                                 holidays. We strive to remain a convenient <br>
+                                 clinic for our patients. To that end, we regularly<br>
+                                 conduct patient surveys and incorporate their feedback.<br>
+                                 Based on many of our patients' feedback, we have<br>
+                                 established a system where patients can arrive <br>
+                                 at our clinic at 8:00 AM on weekdays and have their<br>
+                                 consultation and payment completed by 9:00 AM, allowing<br>
+                                 them to prioritize their work and life commitments.<br>
+                                 Additionally, for those who wish to leave the clinic <br>
+                                 earlier than 9:00 AM, we offer a system where patients <br>
+                                 can leave immediately after their blood draw and pelvic<br>
+                                 exam and receive the results and treatment plan via phone <br>
+                                 the same day, or in the evening after work.<br>
+                                 Since egg collection and implantation require patients <br>
+                                 to arrive at designated times, we have incorporated<br>
+                                 feedback from our patient surveys to minimize the <br>
+                                 need to take time off work. Furthermore, if samples <br>
+                                 are prepared between 8:00 AM and 7:00 PM, we are able <br>
+                                 to provide artificial insemination, semen analysis, <br>
+                                 and sperm freezing services until 8:00 PM.<br>
+                          </span>
+                      </div>
+
+
+                        <div class="box2">
+                        <img src="http://clinic_website.test/wp-content/uploads/2025/12/doctor.webp" alt="">
+                        </div>
+                     
+                   </div>
+
+           </div>
+         
+
+</div>              
+
+
 </main>
+
+
 
 <?php get_footer(); ?>
